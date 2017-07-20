097a2902c2f87cf5dd7966b536eb037916e67a8f
Aaron Ploetz <aaron.ploetz@target.com>
2017-07-20 12:32:00 -0500

    cutting Python edge writes down to a 3 parameterized queries, instead of 3 individual.

--- neo4jISS52.py
+++ neo4jISS52.py
@@ -16,19 +16,11 @@ session.run(createMission,{"name":"ISS-51/52 (Soyuz)"})
 
 #CREATE FLOWN_ON edges for astronauts (below) to Expedition 52
 createRelationship = """MATCH (m:Mission),(a:Astronaut)
-    WHERE m.name='ISS-51/52 (Soyuz)' AND a.name='Jack D. Fischer'
+    WHERE m.name={mname} AND a.name={aname}
     CREATE (a)-[:FLEW_ON]->(m);"""
-session.run(createRelationship)
-
-createRelationship = """MATCH (m:Mission),(a:Astronaut)
-    WHERE m.name='ISS-51/52 (Soyuz)' AND a.name='Peggy A. Whitson'
-    CREATE (a)-[:FLEW_ON]->(m);"""
-session.run(createRelationship)
-
-createRelationship = """MATCH (m:Mission),(a:Astronaut)
-    WHERE m.name='ISS-51/52 (Soyuz)' AND a.name='Randolph J. Bresnik'
-    CREATE (a)-[:FLEW_ON]->(m);"""
-session.run(createRelationship)
+session.run(createRelationship,{"mname":"ISS-51/52 (Soyuz)","aname":"Jack D. Fischer"})
+session.run(createRelationship,{"mname":"ISS-51/52 (Soyuz)","aname":"Peggy A. Whitson"})
+session.run(createRelationship,{"mname":"ISS-51/52 (Soyuz)","aname":"Randolph J. Bresnik"})
 
 #query the mission and the edges to the Astronauts
 queryRelationship = """MATCH (m:Mission {name:'ISS-51/52 (Soyuz)'})<-[:FLEW_ON]-
